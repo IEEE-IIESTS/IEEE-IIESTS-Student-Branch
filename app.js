54a5aa572c5c0181fb5aa160545b5e887dbebf57
Diptarka Roy <diptarkaroy2@gmail.com>
2022-02-05 11:55:14 +0530

    modified app.js

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ var express = require('express'),
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
+app.get("/", function (req, res) {
+    res.render("home/home");
+})
+app.get("/underCons", function (req, res) {
+    res.render("under-construction/Under-Construction");
+})
 
 app.listen(3000, function () {
     console.log("Server has started on port 3000");
