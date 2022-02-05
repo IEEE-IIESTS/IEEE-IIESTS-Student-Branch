cb059c426b8811ba8aad823a5bf90368c3dce0df
Diptarka Roy <diptarkaroy2@gmail.com>
2022-02-05 11:50:34 +0530

    Moved .css and .js files to public folder

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express'),
     app = express();
 
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+
 app.listen(3000, function () {
     console.log("Server has started on port 3000");
 })
