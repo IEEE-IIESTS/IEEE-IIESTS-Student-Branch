55866a7a4b41d2192b7c4d523d18f6596a11a133
Diptarka Roy <diptarkaroy2@gmail.com>
2022-06-19 10:10:44 +0530

    configured port to support dynamic ports

--- app.js
+++ app.js
@@ -23,6 +23,6 @@ app.get("*", function (req, res) {
   res.render("Under-Construction/Under-Construction");
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Server has started on port 3000");
+app.listen(process.env.PORT, function () {
+  console.log(`Server has started on port ${process.env.PORT}`);
 });
