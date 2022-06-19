accf4aa8c8970599427741488f8439e8cd3d9204
Diptarka Roy <diptarkaroy2@gmail.com>
2022-06-19 11:02:11 +0530

    minor fixes

--- app.js
+++ app.js
@@ -23,6 +23,6 @@ app.get("*", function (req, res) {
   res.render("Under-Construction/Under-Construction");
 });
 
-app.listen(process.env.PORT, function () {
+app.listen(process.env.PORT||3000, function () {
   console.log(`Server has started on port ${process.env.PORT}`);
 });
